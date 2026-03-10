b1c5b4fed57bf3d24499747be773d2c515512a15
hoon3948 <robot3949@gmail.com>
2026-03-10 15:35:05 +0900

    feat: dto 생성

---
+++ src/main/java/kr/spartaclub/cloudsparta/member/dto/MemberResponse.java
@@ -0,0 +1,19 @@
+package kr.spartaclub.cloudsparta.member.dto;
+
+import kr.spartaclub.cloudsparta.member.entity.Member;
+import lombok.Getter;
+
+@Getter
+public class MemberResponse {
+    private final Long id;
+    private final String name;
+    private final int age;
+    private final String mbti;
+
+    public MemberResponse(Member member) {
+        this.id = member.getId();
+        this.name = member.getName();
+        this.age = member.getAge();
+        this.mbti = member.getMbti();
+    }
+}
